08402c53e88e97d5cde1f2147145beb86c5089c1
Michael Chen <miketychen@gmail.com>
2016-02-17 10:45:09 -0600

    add new function: location same as day

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,11 @@ $( window ).load(function() {
 	var numDays = 5;
 	var positions = [];
 	var scrollTop = $(window).scrollTop();
+	var cities = ["SEATTLE","LA","SF","CHICAGO","HOUSTON"];
 	for(var x = numDays; x>0;x--){
 		var top = $('#day-'+x).position().top-100;
 		changeDays(scrollTop,top,x);
+		changeCity(scrollTop,top,x);
 		positions.push(top);
 	}
 
@@ -12,12 +14,19 @@ $( window ).load(function() {
 		scrollTop = $(window).scrollTop();
 		$.each(positions, function( index, value ) {
 			changeDays(scrollTop,value,numDays-index);
+			changeCity(scrollTop,value,numDays-index-1);
 		});
 	});
 	function changeDays(scrollTop,value,index){		
 		if (scrollTop>=value){
 				day = pad((index), 3 );
 				$("#day-tracker").text('DAY '+day);
+		}
+	};
+	function changeCity(scrollTop,value,index){		
+		if (scrollTop>=value){
+				city = cities[index];
+				$("#city-tracker").text(city);
 		}
 	};
 	function pad (str, max) {
