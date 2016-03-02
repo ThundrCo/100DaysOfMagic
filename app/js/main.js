fc3b12dcb42656ca719302da74f27c99c16697fb
Stella Lee <i.astellalee@gmail.com>
2016-03-02 02:51:23 -0600

    day15 update

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,12 @@
 $( window ).load(function() {
-	var numDays = 14;
+	var numDays = 15;
 	var positions = [];
 	var scrollTop = $(window).scrollTop();
 	var cities = ["NEW YORK CITY","STRASBOURG, FRANCE","DOWNTOWN LOS ANGELES",
 	"NEW YORK CITY","SEATTLE, WASHINGTON","SEATTLE, WASHINGTON", "DOWNTOWN SEATTLE", 
 	"GRANADA, SPAIN", "GRANADA, SPAIN", "GRANADA, SPAIN", "STRASBOURG, FRANCE",
-	"STRASBOURG, FRANCE", "FEZ, MOROCCO", "STRASBOURG, FRANCE"];
+	"STRASBOURG, FRANCE", "FEZ, MOROCCO", "STRASBOURG, FRANCE", 
+	"TSIM SHA TSUI, HONG KONG"];
 	
 	for(var x = numDays; x>0;x--){
 		var top = $('#day-'+x).position().top-100;
